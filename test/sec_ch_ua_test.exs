bd0260dcd6b5ea77d1503256d4fd74e985e7f52d
Jake Morrison <jake@cogini.com>
2021-02-22 09:56:24 +0800

    Add live test examples

--- test/sec_ch_ua_test.exs
+++ test/sec_ch_ua_test.exs
@@ -6,10 +6,25 @@ defmodule SecChUaTestt do
 
   alias HttpStructuredField.Parser
 
-  test "Sec-CH-UA header" do
-    assert {:ok, [
-      {:string, "Examplary Browser", [{"v", {:string, "73"}}]},
-      {:string, ";Not?A.Brand", [{"v", {:string, "27"}}]}
-    ]} == Parser.parse(~S<"Examplary Browser"; v="73", ";Not?A.Brand"; v="27">)
+  describe "Sec-CH-UA header" do
+    test "RFC example" do
+      assert {:ok, [
+        {:string, "Examplary Browser", [{"v", {:string, "73"}}]},
+        {:string, ";Not?A.Brand", [{"v", {:string, "27"}}]}
+      ]} == Parser.parse(~S<"Examplary Browser"; v="73", ";Not?A.Brand"; v="27">)
+    end
+
+    test "Live examples" do
+      assert {:ok, [
+        {:string, "Google Chrome", [{"v", {:string, "87"}}]},
+        {:string, " Not;A Brand", [{"v", {:string, "99"}}]},
+        {:string, "Chromium", [{"v", {:string, "87"}}]}
+      ]} == Parser.parse("\"Google Chrome\";v=\"87\", \" Not;A Brand\";v=\"99\", \"Chromium\";v=\"87\"")
+      assert {:ok, [
+        {:string, "Chromium", [{"v", {:string, "88"}}]},
+        {:string, "Google Chrome", [{"v", {:string, "88"}}]},
+        {:string, ";Not A Brand", [{"v", {:string, "99"}}]}
+      ]} == Parser.parse("\"Chromium\";v=\"88\", \"Google Chrome\";v=\"88\", \";Not A Brand\";v=\"99\"")
+    end
   end
 end
